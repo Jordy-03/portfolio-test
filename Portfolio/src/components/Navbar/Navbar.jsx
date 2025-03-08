80602a7b72afe55c7310dfab422984bdee0878eb
Jordy Ruiz <jr03@uw.edu>
2025-03-07 21:16:10 -0800

    Finished implementation of Nav.

--- Portfolio/src/components/Navbar/Navbar.jsx
+++ Portfolio/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css'
-import {getImageUrl} from '../../util.js'
+// import {getImageUrl} from '../../util.js'
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <nav className={styles.navbar}>
-            <a className={styles.title} href="/">Portfolio</a>
+            <a className={styles.title} href="/">Jordy Ruiz</a>
 
             <div className={styles.menu}>
-                <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu button/"></img>
-                <ul className={styles.menuItems}>
+                <img
+                    className={styles.menuBtn}
+                    src={menuOpen ? "/assets/nav/closeIcon.png" : "/assets/nav/menuIcon.png"}
+                    alt="menu button/"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                </img>
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}>
                     <li>
-                        <a href="#about">about</a>
+                        <a href="#about">About</a>
                     </li>
                     <li>
-                        <a href="#experience">experience</a>
+                        <a href="#experience">Experience</a>
                     </li>
                     <li>
-                        <a href="#projects">projects</a>
+                        <a href="#projects">Projects</a>
                     </li>
                     <li>
-                        <a href="#contact">contact</a>
+                        <a href="#contact">Contact</a>
                     </li>
                 </ul>
             </div>
