b93b1e35fe3dc177a31698d374c479d808cc1f2f
Jordy Ruiz <jr03@uw.edu>
2025-03-27 23:32:08 -0700

    Beginning work on the About page

--- Portfolio/src/Pages/About.jsx
+++ Portfolio/src/Pages/About.jsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import styles from '../Modules/About.module.css'
+
 export const About = () => {
-    
+    return (
+        <section className={styles.container}>
+            
+        </section>
+    )
 }
