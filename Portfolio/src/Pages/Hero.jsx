b1e229579de10c5251d89a505df3f834db2b303b
Jordy Ruiz <jr03@uw.edu>
2025-03-28 19:48:00 -0700

    Modified font to pt-serif

--- Portfolio/src/Pages/Hero.jsx
+++ Portfolio/src/Pages/Hero.jsx
@@ -5,11 +5,12 @@ export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Hi, my name is Jordy!</h1>
+                <h1 className={styles.title}>
+                    <span className={styles.emoji}>👋</span> Hola, me llamo <u>Jordy</u>!
+                </h1>
                 <p className={styles.description}>
-                    I am a CSSE graduate from the University of Washington
-                    with a passion for creating efficient and user-friendly web
-                    applications! Please reach out if you'd like to learn more about my work.
+                    I am someone with a passion for creating efficient 
+                    and user-friendly web applications.
                 </p>
                 <a className={styles.contactBtn} href="mailto:jor.rui03@gmail.com">Contact Me</a>
             </div>
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import './index.css';
+import App from './App.jsx';
 
-import "@fontsource/outfit";
-import "@fontsource/roboto";
+// import "@fontsource/outfit";
+// import "@fontsource/roboto";
+import "@fontsource/pt-serif";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
