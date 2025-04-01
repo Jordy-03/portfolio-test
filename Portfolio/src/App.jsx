5f8121507cfbafbd4c027a05e36d0390154b79e6
Jordy Ruiz <jr03@uw.edu>
2025-03-31 20:35:32 -0700

    base

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,26 +1,49 @@
+import { useState, useEffect } from 'react';
 import style from './App.module.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Components
 import { Navbar } from './components/Navbar';
+import { Loader } from './components/Loader';
+
+// Pages
 import { Hero } from './Pages/Hero';
 import { About } from './Pages/About';
 import { Experience } from './Pages/Experience';
 import { Projects } from './Pages/Projects';
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   return (
     <div className={style.App}>
-      <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Experience" element={<Experience />} />
-          <Route path="/Projects" element={<Projects />} />
-        </Routes>
-      </Router>
-      
+      {loading ? (
+        <div>
+          <Loader />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Experience" element={<Experience />} />
+              <Route path="/Projects" element={<Projects />} />
+            </Routes>
+          </Router>
+        </>
+      )
+      }
+
+
     </div>
   )
 }
