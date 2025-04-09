83e810476a21e641f63383cbbc728ab7f31b6337
Jordy Ruiz <jr03@uw.edu>
2025-04-08 23:52:17 -0700

    Added name & email fields

--- Portfolio/src/Pages/Contact.jsx
+++ Portfolio/src/Pages/Contact.jsx
@@ -4,7 +4,11 @@ import styles from '../Modules/Contact.module.css'
 export const Contact = () => {
     return (
         <section className={styles.container}>
-            <h1 className={styles.title}>Contact info</h1>
+            <h1 className={styles.title}>Contact Info</h1>
+            <input type="text" className={styles.input} placeholder="Your Name" />
+            <input type="email" className={styles.input} placeholder="Your Email" />
+
+
         </section>
     )
 }
