8efa1c30cefb3e9c4fb537fdf1bd9a03a8c1b7f8
Jordy Ruiz <jr03@uw.edu>
2025-04-09 23:49:18 -0700

    Aligned content next to each other

--- Portfolio/src/Pages/Contact.jsx
+++ Portfolio/src/Pages/Contact.jsx
@@ -5,16 +5,19 @@ export const Contact = () => {
     return (
         <section className={styles.container}>
             <h1 className={styles.title}>Contact Info</h1>
-            <br/>
-            <p className={styles.information}>
-                Name: Jordy Ruiz<br/>
-                Email: jor.rui03@gmail.com<br/>
-                Phone: (425) 524 - 1981<br/>
-                Location: Renton, WA
-            </p>
-            <input type="text" className={styles.input} placeholder="Your Name" required></input>
-            <input type="email" className={styles.input} placeholder="Your Email" required></input>
-            <textarea className={styles.textarea} placeholder="Your Message"></textarea>
+            <div className={styles.fields}>
+                <p className={styles.information}>
+                    Name: Jordy Ruiz<br />
+                    Email: jor.rui03@gmail.com<br />
+                    Phone: (425) 524 - 1981<br />
+                    Location: Renton, WA
+                </p>
+                <input type="text" className={styles.input} placeholder="Your Name" required></input>
+                <input type="email" className={styles.input} placeholder="Your Email" required></input>
+                <textarea className={styles.textarea} placeholder="Your Message"></textarea>
+            </div>
+
+
         </section>
     )
 }
