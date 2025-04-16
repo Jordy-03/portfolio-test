0906bc6aaaf6ff57caa59d678eba99325d5d6ef7
Jordy Ruiz <jr03@uw.edu>
2025-04-16 00:05:26 -0700

    Stable version of timeline (unfinished)

--- Portfolio/src/Pages/Experience.jsx
+++ Portfolio/src/Pages/Experience.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import styles from '../Modules/Experience.module.css'
+import styles from '../Modules/Experience.module.css';
 
 // import {ReactComponent as WorkIcon} from './work.svg'
 import timelineElements from '../Components/Timeline.jsx';
-import { 
-    VerticalTimeline, 
-    VerticalTimelineElement 
+import {
+    VerticalTimeline,
+    VerticalTimelineElement
 } from "react-vertical-timeline-component";
 
 import "react-vertical-timeline-component/style.min.css";
 
 export const Experience = () => {
     return (
-    <div>
-        <h1>Timeline</h1>
-        <VerticalTimeline>
-            timelineElements.map(element => {
-                return (
-                    <VerticalTimelineElement>
-                        
-                    </VerticalTimelineElement>
-                )
-            });
-        </VerticalTimeline>
-    </div>        
+        <div>
+            <h1>Timeline</h1>
+            <VerticalTimeline> {
+                timelineElements.map((element) => {
+                    // If element icon is work, it will be a work icon
+                    let isWorkIcon = element.icon === "work";
+
+                    return (
+                        <VerticalTimelineElement
+                            key={element.key}
+                            date={element.date}
+                            dateClassName="date"
+                            
+                        >
+                        </VerticalTimelineElement>
+                    );
+                })}
+            </VerticalTimeline>
+        </div>
     );
 }
