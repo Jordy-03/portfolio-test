b1c5b3884a5ee04fd7747326dedfb80886f49e92
Jordy Ruiz <jr03@uw.edu>
2025-04-15 23:19:54 -0700

    Setting up initial Experience Page component

---
+++ Portfolio/src/components/Timeline.jsx
@@ -0,0 +1,13 @@
+let timelineElements = [
+    {
+        id: 1,
+        title: "Undergraduate - Computer Science Major",
+        location: "University of Washington",
+        description: "Completed a Bachelor of Science in Computer Science with a focus on software engineering and web development.",
+        buttonText: "View Frontend Projects",
+        date: "2021 - 2025",
+        icon: "work",
+    }
+]
+
+export default timelineElements;
