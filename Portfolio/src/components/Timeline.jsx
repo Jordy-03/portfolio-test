e2bce53b5b877b38a88cf532efaf4df842246e48
Jordy Ruiz <jr03@uw.edu>
2025-04-17 23:14:42 -0700

    Added additional points of interest in timeline

--- Portfolio/src/components/Timeline.jsx
+++ Portfolio/src/components/Timeline.jsx
@@ -2,10 +2,28 @@ let timelineElements = [
     {
         id: 1,
         title: "Undergraduate - Computer Science Major",
-        location: "University of Washington",
+        location: "University of Washington, Bothell",
         description: "Completed a Bachelor of Science in Computer Science with a focus on software engineering and web development.",
         buttonText: "View Frontend Projects",
         date: "2021 - 2025",
+        icon: "work",
+    },
+    {
+        id: 1,
+        title: "Data Analyst Intern",
+        location: "Bothell, WA",
+        description: "",
+        buttonText: "Created Sankey diagram",
+        date: "2025 - current",
+        icon: "work",
+    },
+    {
+        id: 1,
+        title: "Information Technology Intern",
+        location: "Issaquah, WA",
+        description: "Completed a Bachelor of Science in Computer Science with a focus on software engineering and web development.",
+        buttonText: "View Frontend Projects",
+        date: "2025 - current",
         icon: "work",
     }
 ]
