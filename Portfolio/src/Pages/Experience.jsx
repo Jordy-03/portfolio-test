1d77741f5b30b8fb08f4f4ca183c5aa17cbebfab
Jordy Ruiz <jr03@uw.edu>
2026-02-03 22:48:32 -0800

    Changed Components back to uppercase

--- Portfolio/src/Pages/Experience.jsx
+++ Portfolio/src/Pages/Experience.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../Modules/Experience.module.css';
 
 // import {ReactComponent as WorkIcon} from './work.svg'
-import timelineElements from '../components/Timeline.jsx'; // Updated Compontent folder name
+import timelineElements from '../Components/Timeline.jsx'; // Updated Compontent folder name
 import {
     VerticalTimeline,
     VerticalTimelineElement
